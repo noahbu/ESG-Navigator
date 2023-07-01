fbb564e8f81510f57d9cac8e36b092e3d54fe565
Christophe Schmit <ge69zat@mytum.de>
2023-07-01 18:36:21 +0200

    Added design and logo to all the pages + added dummy dashboard -WIP

--- esg_navigator/frontend/managing_view/Dashboard.py
+++ esg_navigator/frontend/managing_view/Dashboard.py
@@ -7,8 +7,12 @@ import os
 from streamlit_option_menu import option_menu
 import yaml
 from yaml.loader import SafeLoader 
-from esg_navigator.backend.helper import load_css,session_states_init, load_complaints_db,load_manager_data
-from streamlit_chat import message
+from esg_navigator.backend.helper import app_init,load_css, load_complaints_db,load_manager_data
+import datetime
+import plost
+from PIL import Image
+
+
 
 
 st.set_page_config(
@@ -17,62 +21,87 @@ st.set_page_config(
     layout="centered"
 )
 
+image = Image.open('../../design/logo/ucomply_Logo.png')
+st.image(image)
 
-#Session_state_init: 
-#session_states_init()
+app_init()
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-#name, authentication_status, username = authenticator.login('Login', 'main')
-st.session_state['name'], st.session_state['authentication_state'], st.session_state['username'] = authenticator.login('Login', 'main')
-
-if 'authenticator' not in st.session_state:
-    st.session_state['authenticator'] = authenticator
 
 if st.session_state["authentication_status"]:
+
     load_manager_data()
 
     st.title("UComply Dashboard")
     st.write("View your open cases")
     st.dataframe(st.session_state['manager_db'])
+    st.session_state['authenticator'].logout("Logout", "sidebar")
     #st.info("You can now access the app.")
     with st.sidebar:
-        st.session_state['authenticator'].logout("Logout", "sidebar")
-        st.sidebar.title("Ucomply - Your Social Copilot")
-        st.sidebar.write(f"Welcome {st.session_state['username']}!")
+        
+        st.title("Ucomply - Your Social Copilot")
+        st.write(f"Welcome {st.session_state['username']}!")
+    
+        st.header('Dashboard `version 2`')
+
+        st.sidebar.subheader('Heat map parameter')
+        time_hist_color = st.sidebar.selectbox('Color by', ('temp_min', 'temp_max')) 
+
+        st.sidebar.subheader('Donut chart parameter')
+        donut_theta = st.sidebar.selectbox('Select data', ('q2', 'q3'))
+
+        st.sidebar.subheader('Line chart parameters')
+
+        plot_data = st.sidebar.multiselect('Select data', ['temp_min', 'temp_max'], ['temp_min', 'temp_max'])
+        plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+
+        st.sidebar.markdown('''
+        ---
+        Created with ❤️ by [Data Professor](https://youtube.com/dataprofessor/).
+        ''')
+
+
+    # Row A
+    st.markdown('### Metrics')
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
+
+    # Row B
+    seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
+    stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+
+    c1, c2 = st.columns((7,3))
+    with c1:
+        st.markdown('### Heatmap')
+        plost.time_hist(
+        data=seattle_weather,
+        date='date',
+        x_unit='week',
+        y_unit='day',
+        color=time_hist_color,
+        aggregate='median',
+        legend=None,
+        height=345,
+        use_container_width=True)
+    with c2:
+        st.markdown('### Donut chart')
+        plost.donut_chart(
+            data=stocks,
+            theta=donut_theta,
+            color='company',
+            legend='bottom', 
+            use_container_width=True)
+
+    # Row C
+    st.markdown('### Line chart')
+    st.line_chart(seattle_weather, x = 'date', y = plot_data, height = plot_height)
     
 
-    st.write(f"Chat functionality")
-
-    
-    message("My message") 
-    message("Hello bot!", is_user=True)
 
 
 
-
-    
-
-
-    # selected3 = option_menu(None, ["Dashboard", "Complaints",  "Report", 'Settings'], 
-    #             icons=['house', 'bookmarks', "save", 'gear'], 
-    #             menu_icon="cast", default_index=0, orientation="horizontal",
-    #             styles={
-    #                 "container": {"padding": "0!important", "background-color": "#155D59"},
-    #                 "icon": {"color": "orange", "font-size": "20px"}, 
-    #                 "nav-link": {"font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#E9BD47"},
-    #                 "nav-link-selected": {"background-color": "FFCB43"},
-    #             }
-    #         )    
-    
+   
 
     
 elif st.session_state["authentication_status"] is False:
--- esg_navigator/frontend/managing_view/pages/Case_Overview.py
+++ esg_navigator/frontend/managing_view/pages/Case_Overview.py
@@ -11,6 +11,39 @@ if st.session_state["authentication_status"]:
         st.sidebar.write(f"Welcome {st.session_state['username']}!")
 
 
+    st.header(f"Chat functionality")
+
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
+    if "user_input" not in st.session_state:
+        st.session_state['user_input'] = ""
+
+    chat_placeholder = st.empty()
+
+    with chat_placeholder.container():   
+        for i in range(len(st.session_state['chat_history'])):  
+            chat = st.session_state['chat_history'][i]        
+            message(chat["message"], is_user=chat["is_officer"], key=f"{i}_user")
+
+    def on_input_change(is_officer):
+
+        user_input = st.session_state.user_input
+        data = {
+                "timestamp": datetime.datetime.now(),
+                "is_officer": is_officer,
+                "message": user_input
+            }
+        st.session_state.chat_history.append(data)
+        complaint_id = "1234"
+        chat_to_csv(complaint_id, st.session_state.chat_history)
+
+
+    with st.container():
+        st.session_state.user_input = st.text_input("User Input:")
+        if st.button("Submit message"):
+            on_input_change(is_officer = True)
+
+
           # align's the message to the right
 
 
