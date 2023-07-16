814a3ad84956a0697d0dbf3f435f893929d049db
Christophe Schmit <ge69zat@mytum.de>
2023-07-16 10:33:31 +0200

    Edited path

--- esg_navigator/File_a_Report.py
+++ esg_navigator/File_a_Report.py
@@ -6,7 +6,7 @@ import numpy as np
 import secrets
 import pandas as pd
 from datetime import datetime
-from esg_navigator.backend.helper import add_logo
+from backend.helper import add_logo
 import os
 import PyPDF2
 
