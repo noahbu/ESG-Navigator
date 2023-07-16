ca13772105f326a5bc780792962b6ca9230ee301
Christophe Schmit <ge69zat@mytum.de>
2023-07-16 11:10:14 +0200

    Edited path

---
+++ esg_navigator/Dashboard.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import pickle
+from pathlib import Path
+import pandas as pd
+import os
+from streamlit_option_menu import option_menu
+import yaml
+from yaml.loader import SafeLoader 
+from esg_navigator.backend.helper import app_init,load_css, load_complaints_db, load_manager_data
+import datetime
+import plost
+from PIL import Image
+
+
+st.set_page_config(
+    page_title=" Welcome to Ucomply - Your Social Copilot",
+    page_icon="👋",
+    layout="centered"
+)
+
+#load logo section
+image = Image.open('/design/logo/ucomply_Logo.png')
+st.image(image)
+
+#Variable initialization & login functionality
+app_init()
+
+#Only display text if loggeed in:
+if st.session_state["authentication_status"]:
+    #Write Title
+    st.title("Integrity Intelligence Dashboard")
+    st.write("")
+
+    #Display logout button in the sidebar button
+    st.session_state['authenticator'].logout("Logout", "sidebar")
+
+    #Read filtered complaints_db according to your login:
+    load_manager_data()
+    db = st.session_state['manager_db']
+
+
+    #Customize sidebar
+    with st.sidebar:
+        
+        #Display username in the sidebar
+        st.title("Ucomply - Your Social Copilot")
+        st.write(f"Welcome {st.session_state['username']}!")
+
+        st.markdown('''
+        ---
+        Created with ❤️ by UComply.
+        ''')
+
+
+    #Dashboard Build: ROW 1
+    col1, col2, col3 = st.columns(3)
+
+    df_status = db.groupby('Status').size().reset_index(name='Occurrences')
+    count_new = df_status[df_status['Status'] == 'New']['Occurrences']
+
+    #Get percentage of new cases in percentage in comparison to 
+    col1.metric("New open cases", count_new, "10%")
+
+
+    # This months complaints 
+    col2.metric("This months complaints", " 15", "33%")
+    #Filter open complaints by region for Germany
+ 
+    org_byregion = db.groupby('Region').size().reset_index(name='Occurrences')
+    counts_byregion = org_byregion[org_byregion['Region'] == 'Germany']['Occurrences']
+    col3.metric("Complaints Location: Germany ", counts_byregion, "10%")
+
+    #Dashboard Build: ROW 2    
+
+    c1, c2 = st.columns((5,5), gap = "large")
+    with c1:
+        st.markdown('### Cases by Location')
+        df_region = db.groupby('Region').size().reset_index(name='Occurrences')
+        plost.bar_chart(
+        data=df_region,
+        bar = 'Region',
+        value='Occurrences',
+        color = "#155D59",
+        use_container_width=True,
+        height = 350,
+        direction='horizontal')
+
+    with c2:
+        #Display the Donut active status chart
+
+        #Color encoding for the donut chart
+        encoding = {
+            "field": "Status", 
+            "type": "nominal",
+            "scale": {
+                "domain": ["Closed", "Pending", "New"],
+                "range": ["#155D59", "#0C6D69", "#FFCB43"]
+            }}
+
+        st.markdown('### Cases by Status')
+        #Plot the donut chart
+        plost.donut_chart(
+            data=df_status,
+            theta = "Occurrences",
+            color = encoding,
+            height = 350,
+            legend='bottom', 
+            use_container_width=True)
+
+    #Dashboard Build: ROW 3
+    #Create new db for bar chart where we do groupby Category and sum up its Occurrences: 
+    df_bar = db.groupby('Category').size().reset_index(name='Occurrences')
+    st.markdown('### Cases by Report Category')
+    #Plot the bar chart
+    plost.bar_chart(
+        data=df_bar,
+        bar = 'Category',
+        value='Occurrences',
+        color = "#155D59",
+        use_container_width=True)
+
+#Display error message if login is incorrect
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+#Display warning message if no login is entered
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+
+
+
+  
---
+++ esg_navigator/pages/Case_Overview.py
@@ -0,0 +1,108 @@
+import streamlit as st
+from streamlit_chat import message
+import datetime
+from esg_navigator.backend.helper import add_logo, chat_to_csv, load_chat_history, load_manager_data, on_input_change
+#from random_username.generate import generate_username
+
+st.set_page_config(
+    page_title=" Welcome to Ucomply - Your Social Copilot",
+    page_icon="👋",
+    layout="centered"
+)
+#Load logo to the sidebar
+add_logo()
+
+
+
+#Intialize session state variables
+if "user_input" not in st.session_state:
+        st.session_state['user_input'] = ""
+
+
+
+
+if st.session_state["authentication_status"]:
+    #Sidebar customization
+    with st.sidebar:
+        st.session_state['authenticator'].logout("Logout", "sidebar")
+        st.sidebar.title("Ucomply - Your Social Copilot")
+        st.sidebar.write(f"Welcome {st.session_state['username']}!")
+        st.markdown('''
+        ---
+        Created with ❤️ by UComply.
+        ''')
+
+
+
+    st.title('Manage your active cases Cases')
+
+    st.header("Overview of your active cases")
+    # This months complaints 
+    st.metric("You have new unread messages:", " 3")
+    #Filter open complaints by region for Germany
+
+
+
+    st.header("Displaying all active cases")
+
+    st.write("View your open cases")
+
+
+   
+
+    #Load the database into the session state
+    load_manager_data()
+    #Get the database from the session state
+    db = st.session_state['manager_db']
+
+    #Display editable table
+    st.data_editor(st.session_state['manager_db'])
+
+    st.write("Select a case to view the status of your report")
+    #Display the status of the case
+    complainee = st.empty()
+
+    #Drop-down for complaint handling
+    case = complainee.selectbox("Select a case", st.session_state['manager_db']['case-name'].unique())
+
+
+    st.write(case)
+
+    st.divider()
+
+    #Chat functionality
+    st.header(f"Complainee Interaction")
+    st.write("Chat with the complainee to resolve the case")
+    st.write("")
+    case_id = db.loc[db['case-name'] == case, 'ID'].values[0]
+
+    #Load chat_history forthe case in question
+    load_chat_history(case_id)    
+    #Display chat history
+    chat_placeholder = st.empty()
+    #Load chat history
+    with chat_placeholder.container():   
+        for i in range(len(st.session_state['chat_history'])):  
+            chat = st.session_state['chat_history'][i]        
+            message(chat["message"], is_user = chat["is_officer"], key=f"{i}_user")
+
+    #Text functionality
+    with st.container():
+        st.session_state.user_input = st.text_input("Generate your Response:")
+        if st.button("Submit message"):
+            on_input_change(case_id, is_officer = True)
+            st.experimental_rerun()
+
+
+
+          # align's the message to the righ
+
+
+
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+
+        
